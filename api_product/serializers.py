f07e5f2d7354ff8b21bdb2b1255ab7b17a1a46cd
kuka666 <kuka666@github.com>
2022-05-11 03:50:19 +0600

    category in serializers

--- api_product/serializers.py
+++ api_product/serializers.py
@@ -1,46 +1,46 @@
 # tokensite/blog/serializers.py
 
 from rest_framework.serializers import (
-	HyperlinkedIdentityField,
+    HyperlinkedIdentityField,
 )
 
 from rest_framework import serializers
 
 from rest_framework.serializers import (
-      ModelSerializer,
+    ModelSerializer,
 )
 from rest_framework.response import Response
 
 from product.models import Product, Order, Cart
 
 add_cart_url = HyperlinkedIdentityField(
-	view_name = 'api-product:api-create-cart',
-	lookup_field = 'slug'
+    view_name='api-product:api-create-cart',
+    lookup_field='slug'
 )
 
+
 class postSerializer(ModelSerializer):
-	cart_url = add_cart_url
-	class Meta:
-		model = Product
-		fields = ['id',
-			'title',
-			'desc',
-			'price',
-			'image',
-			'cart_url',
-			'slug',
-			]
+    cart_url = add_cart_url
 
-
+    class Meta:
+        model = Product
+        fields = ['id',
+                  'title',
+                  'desc',
+                  'price',
+                  'image',
+                  'cart_url',
+                  'slug',
+                  'category'
+                  ]
 
 
 class cartSerializer(ModelSerializer):
-	product = postSerializer()
-	class Meta:
-		model = Cart
-		fields = [
-			'id',
-			'product',
-			]
+    product = postSerializer()
 
-
+    class Meta:
+        model = Cart
+        fields = [
+            'id',
+            'product',
+        ]
